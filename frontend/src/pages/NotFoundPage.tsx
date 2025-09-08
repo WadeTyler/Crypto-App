4d0dc3fbb7de2c2466893dff43db9c9840d0a4e0
Tyler Wade <vaxonreal@gmail.com>
2025-09-08 16:42:45 -0400

    enhance NotFoundPage with improved styling and navigation link

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,8 +1,16 @@
+import {Link} from "react-router";
 
 export default function NotFoundPage() {
   return (
-    <div>
-      404 - Page Not Found
+    <div className="page">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-8">
+        <h1 className="text-3xl font-semibold text-accent text-shadow-md text-shadow-accent/20 text-center text-balance">
+          Sorry, we couldn't find that page.
+        </h1>
+        <Link to="/" className="btn-1 w-fit">
+          Back Home
+        </Link>
+      </div>
     </div>
   )
 }
